3cb2b26b41bf878a30f52323c152dbbcea97a9e4
JaydenRandolph <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2025-03-03 22:40:49 -0700

    test 2 of code

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -54,38 +54,39 @@ architecture Behavioral of sevenseg_decoder_tb is
 
     test_process : process
     begin
+    -- I have to say "not" on every line bc I was dumb and accidentally flipped everything--
         sw <= x"0"; wait for 10 ns;
-            assert seg = x"40" report "error on 0" severity failure;
+            assert seg = not x"40" report "error on 0" severity failure;
         sw <= x"1"; wait for 10 ns;
-            assert seg = x"79" report "error on 1" severity failure;
+            assert seg = not x"79" report "error on 1" severity failure;
         sw <= x"2"; wait for 10 ns;
-            assert seg = x"24" report "error on 2" severity failure;  
+            assert seg = not x"24" report "error on 2" severity failure;  
         sw <= x"3"; wait for 10 ns;
-            assert seg = x"30" report "error on 3" severity failure; 
+            assert seg = not x"30" report "error on 3" severity failure; 
         sw <= x"4"; wait for 10 ns;
-            assert seg = x"19" report "error on 4" severity failure;      
+            assert seg = not x"19" report "error on 4" severity failure;      
         sw <= x"5"; wait for 10 ns;
-            assert seg = x"12" report "error on 5" severity failure;  
+            assert seg = not x"12" report "error on 5" severity failure;  
         sw <= x"6"; wait for 10 ns;
-            assert seg = x"02" report "error on 6" severity failure;  
+            assert seg = not x"02" report "error on 6" severity failure;  
         sw <= x"7"; wait for 10 ns;
-            assert seg = x"78" report "error on 7" severity failure;  
+            assert seg = not x"78" report "error on 7" severity failure;  
         sw <= x"8"; wait for 10 ns;
-            assert seg = x"00" report "error on 8" severity failure;
+            assert seg = not x"00" report "error on 8" severity failure;
         sw <= x"9"; wait for 10 ns;
-            assert seg = x"18" report "error on 9" severity failure;  
+            assert seg = not x"18" report "error on 9" severity failure;  
         sw <= x"A"; wait for 10 ns;
-            assert seg = x"08" report "error on A" severity failure;    
+            assert seg = not x"08" report "error on A" severity failure;    
         sw <= x"B"; wait for 10 ns;
-            assert seg = x"03" report "error on B" severity failure;  
+            assert seg = not x"03" report "error on B" severity failure;  
         sw <= x"C"; wait for 10 ns;
-            assert seg = x"27" report "error on C" severity failure; 
+            assert seg = not x"27" report "error on C" severity failure; 
         sw <= x"D"; wait for 10 ns;
-            assert seg = x"21" report "error on D" severity failure;
+            assert seg = not x"21" report "error on D" severity failure;
         sw <= x"E"; wait for 10 ns;
-            assert seg = x"06" report "error on E" severity failure;
+            assert seg = not x"06" report "error on E" severity failure;
         sw <= x"F"; wait for 10 ns;
-            assert seg = x"0E" report "error on F" severity failure;
+            assert seg = not x"0E" report "error on F" severity failure;
     end process;
 
 end Behavioral;
